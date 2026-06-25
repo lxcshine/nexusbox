4516f18a8da397866929fc448f8f81ada69097ea
lxcshine <97235189+lxcshine@users.noreply.github.com>
2026-06-25 16:55:33 +0800

    test(api): add unit tests for API type validation

--- pkg/apis/sandbox/v1alpha1/types_test.go
+++ pkg/apis/sandbox/v1alpha1/types_test.go
@@ -1,10 +1,3 @@
-/*
-Copyright 2024 NexusBox Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-*/
-
 package v1alpha1
 
 import (
